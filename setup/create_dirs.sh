a221c76f620fee7e869760f5804933dd81ec9742
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-04-17 09:23:11 +0200

    - first approach on docker compose - still doesn't work on mac - long road behind ...

---
+++ setup/create_dirs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+mkdir -p ~/jalgoarena/zookeeper/zoo1/data
+mkdir -p ~/jalgoarena/zookeeper/zoo1/datalog
+
+mkdir -p ~/jalgoarena/kafka/kafka1/data
+mkdir -p ~/jalgoarena/kafka/kafka2/data
+mkdir -p ~/jalgoarena/kafka/kafka3/data
+
+mkdir -p ~/jalgoarena/users/
+mkdir -p ~/jalgoarena/submissions/
+mkdir -p ~/jalgoarena/ranking/
