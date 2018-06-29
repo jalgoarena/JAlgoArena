1e3c7b93c52eda708fec11b8e1c3c36247bf61a9
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-06-29 18:35:51 +0200

    - switch out of Spring Boot Zuul to traefik (via binary / docker) - update run_all script - add traefik configuration - fix gradle scripts for Spring Boot 2 - remove cache controlers

--- remove_all.sh
+++ remove_all.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 echo "Cleanup existing containers"
-docker stop consul
-docker stop zookeeper
-docker stop kafka1
-docker stop kafka2
-docker stop kafka3
-docker stop jalgoarena-api
+docker stop jalgoarena-ui
 docker stop jalgoarena-queue
 docker stop jalgoarena-events
 docker stop jalgoarena-judge-1
@@ -14,13 +9,19 @@ docker stop jalgoarena-judge-3
 docker stop jalgoarena-auth
 docker stop jalgoarena-submissions
 docker stop jalgoarena-ranking
+docker stop traefik
+docker stop kafka1
+docker stop kafka2
+docker stop kafka3
+docker stop zookeeper
+docker stop consul
 
 docker rm consul
 docker rm zookeeper
 docker rm kafka1
 docker rm kafka2
 docker rm kafka3
-docker rm jalgoarena-api
+docker rm traefik
 docker rm jalgoarena-queue
 docker rm jalgoarena-events
 docker rm jalgoarena-judge-1
@@ -29,3 +30,4 @@ docker rm jalgoarena-judge-3
 docker rm jalgoarena-auth
 docker rm jalgoarena-submissions
 docker rm jalgoarena-ranking
+docker rm jalgoarena-ui
--- run_all.sh
+++ run_all.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-docker run -d --net=host --name=consul -v /home/jacek/github/JAlgoArena/consul:/consul/config -e CONSUL_UI_BETA=true consul agent -server -ui -bootstrap-expect=1 -bind=192.168.63.21
+docker run -d --net=host --name=consul -v $HOME/jalgoarena/consul:/consul/config -e CONSUL_UI_BETA=true consul agent -server -ui -bootstrap-expect=1 -bind=192.168.63.21
 echo "Consul started"
 docker run -d --net=host --name zookeeper -e ZOOKEEPER_CLIENT_PORT=2181 confluentinc/cp-zookeeper
 echo "Zookeeper started"
@@ -11,7 +11,7 @@ docker run -d --net=host --name kafka3 -e KAFKA_BROKER_ID=2 -e KAFKA_ADVERTISED_
 echo "Kafka 3 started"
 
 echo "Starting services"
-docker run -d --net=host --name=jalgoarena-api spolnik/jalgoarena-api:latest
+docker run -d --net=host --name=traefik -v $HOME/jalgoarena/traefik.toml:/etc/traefik/traefik.toml traefik
 echo "API Started"
 docker run -d --net=host --name=jalgoarena-queue  -e BOOTSTRAP_SERVERS=localhost:9092,localhost:9093,localhost:9094 spolnik/jalgoarena-queue:latest
 echo "Queue Started"
@@ -23,11 +23,11 @@ docker run -d --net=host --name=jalgoarena-judge-2 -e PORT=6001 -e BOOTSTRAP_SER
 echo "Judge 2 Started"
 docker run -d --net=host --name=jalgoarena-judge-3 -e PORT=6002 -e BOOTSTRAP_SERVERS=localhost:9092,localhost:9093,localhost:9094 spolnik/jalgoarena-judge:latest
 echo "Judge 3 Started"
-docker run -d --net=host --name=jalgoarena-auth -v /home/jacek/github/JAlgoArena-Auth/UserDetailsStore:/app/UserDetailsStore spolnik/jalgoarena-auth:latest
+docker run -d --net=host --name=jalgoarena-auth -v $HOME/jalgoarena/UserDetailsStore:/app/UserDetailsStore spolnik/jalgoarena-auth:latest
 echo "Auth Started"
-docker run -d --net=host --name=jalgoarena-submissions -v /home/jacek/github/JAlgoArena-Submissions/SubmissionsStore:/app/SubmissionsStore spolnik/jalgoarena-submissions:latest
+docker run -d --net=host --name=jalgoarena-submissions -v $HOME/jalgoarena/SubmissionsStore:/app/SubmissionsStore spolnik/jalgoarena-submissions:latest
 echo "Submissions Started"
-docker run -d --net=host --name=jalgoarena-ranking -v /home/jacek/github/JAlgoArena-Ranking/RankingStore:/app/RankingStore spolnik/jalgoarena-ranking:latest
+docker run -d --net=host --name=jalgoarena-ranking -v $HOME/jalgoarena/RankingStore:/app/RankingStore spolnik/jalgoarena-ranking:latest
 echo "Ranking Started"
 
 echo "Starting UI"
