2382726b35bc57bc0877ace9422ef88577d69111
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-07-18 15:25:48 +0200

    - move all configs under nomad dir

--- kafka/create_topics.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-export PATH=/Users/jacek/kafka_2.12-1.1.0/bin:$PATH
-kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 3 --partitions 9 --topic submissions
-kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 2 --partitions 2 --topic results
-kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 2 --partitions 2 --topic events
