80455c9d12f39e53a386bbfdccf1916b5e2245cf
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-07-19 10:30:31 +0200

    - fix step0.sh

--- nomad/step0.sh
+++ nomad/step0.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker run -d --net=host --name=consul -e CONSUL_UI_BETA=true consul agent -server -ui -bootstrap-expect=1 -bind=192.168.63.21
-nomad agent -config service.hcl
+nomad agent -config server.hcl
